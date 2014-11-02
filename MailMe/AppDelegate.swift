f4a04665f68905d4741ccd13e98a410005fe5cff
dignifiedquire <dignifiedquire@gmail.com>
2014-11-02 15:17:26 +0100

    Initial Commit

---
+++ MailMe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MailMe
+//
+//  Created by Friedel Ziegelmayer on 02/11/14.
+//  Copyright (c) 2014 Friedel Ziegelmayer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MailMe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MailMe
+//
+//  Created by Friedel Ziegelmayer on 02/11/14.
+//  Copyright (c) 2014 Friedel Ziegelmayer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
